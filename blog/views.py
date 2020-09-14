2ccd28b907f988d550e391b4b064adc00eec1ee9
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-09-14 21:50:13 +0300

    Replaced the function for displaying the list of articles with class.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    #  Вместо задания атрибута QuerySet мы могли бы указать модель model=Post,
+    #  и тогда Django, используя стандартный менеджер модели, получал бы объекты как Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    #  Базовый обработчик Django ListView передает объект в качестве переменной с именем page_obj
 
 
 def post_list(request):
@@ -14,6 +25,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    # Вообще, при удалении из словаря "page", в работе блога ничего не меняется (на этапе создания пагинатора)
 
 
 def post_detail(request, year, month, day, post):
