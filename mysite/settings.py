1c4ce601fe110769c323e36de3c60e4d8c0f1918
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-09-15 23:37:25 +0300

    reset

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #  Для использования SMTP-сервера gmail
+#  Также необходимо включить в настройках аккаунта https://myaccount.google.com/lesssecureapps
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ergo.cassum@gmail.com'
-EMAIL_HOST_PASSWORD = 'long password'
+EMAIL_HOST_USER = 'nickname@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
