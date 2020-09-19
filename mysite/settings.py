613cd8b4e3ca984c5cad4322b75cccf09cb3448a
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-09-19 22:23:11 +0300

    Add comment model.

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,7 +51,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
-#TEMPLATE_DIR = os.path.join(BASE_DIR)
+#  TEMPLATE_DIR = os.path.join(BASE_DIR)
 
 TEMPLATES = [
     {
