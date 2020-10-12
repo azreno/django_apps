6336fff9a944e4ee25e298da7856c059b07159a6
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-10-12 21:28:29 +0300

    Add rank search.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 class PostListView(ListView):
@@ -90,5 +90,10 @@ def post_search(request):
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
-        results = Post.objects.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+        search_vector = SearchVector('title', 'body')
+        search_query = SearchQuery(query)
+        results = Post.objects.annotate(
+            search=search_vector,
+            rank=SearchRank(search_vector, search_query)
+        ).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
