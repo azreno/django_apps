15aa1debb307a9ba76a0c4ba681fe70fdd1a19b1
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-10-14 23:18:58 +0300

    Add trigram search for post titles.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 
 class PostListView(ListView):
@@ -90,10 +90,14 @@ def post_search(request):
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
-        search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-        search_query = SearchQuery(query)
+        # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+        # search_query = SearchQuery(query)
+        # results = Post.objects.annotate(
+        #     search=search_vector,
+        #     rank=SearchRank(search_vector, search_query)
+        # ).filter(rank__gte=0.3).order_by('-rank')
+        # TODO: Add trigram search for body
         results = Post.objects.annotate(
-            search=search_vector,
-            rank=SearchRank(search_vector, search_query)
-        ).filter(rank__gte=0.3).order_by('-rank')
+            similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
